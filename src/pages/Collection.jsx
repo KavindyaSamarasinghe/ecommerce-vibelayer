4b59e8317dd4d9d619e9b2a203d82334f613453f
KavindyaSamarasinghe <kavindyasamara@gmail.com>
2025-04-14 18:50:11 +0530

    type

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -44,6 +44,30 @@ const Collection = () => {
       </div>
     </div>
 
+    {/* subcategiry filters */}
+
+    <div className={`border border-gray-300 pl-5 py-3 mt-6 ${
+      showFilter ? 'block' : 'hidden'
+    } sm:block`}>
+      <p className='mb-3 text-sm font-medium'>TYPE</p>
+      <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
+        <p className='flex gap-2'>
+          <input className='w-3' type="checkbox" value={'Topwear'} />Topwear
+        </p>
+
+        <p className='flex gap-2'>
+          <input className='w-3' type="checkbox" value={'Bottomwear'} />Bottomwear
+        </p>
+
+        <p className='flex gap-2'>
+          <input className='w-3' type="checkbox" value={'Winterwear'} />wimterwear
+          </p>
+
+      </div>
+    </div>
+
+
+
       </div>
 
       
