646c112189512f402c67e98b2c819729b5dc7d5d
KavindyaSamarasinghe <kavindyasamara@gmail.com>
2025-04-14 19:54:06 +0530

    product sort

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { ShopContext } from '../context/ShopContext';
+import { assets } from '../assets/assets';
+import Title from '../components/Title';
+
 
 const Collection = () => {
 
@@ -21,6 +24,9 @@ const Collection = () => {
         onClick={() => setShowFilter(!showFilter)}
       >
         FILTERS
+        <img 
+  className={`h-3 sm:hidden ${showFilter ? 'rotate-90' : ''}`} src={assets.dropdown_icon} alt="" />
+
       </p>
       
       {/* Category filters */}
@@ -46,7 +52,7 @@ const Collection = () => {
 
     {/* subcategiry filters */}
 
-    <div className={`border border-gray-300 pl-5 py-3 mt-6 ${
+    <div className={`border border-gray-300 pl-5 py-3 my-5 ${
       showFilter ? 'block' : 'hidden'
     } sm:block`}>
       <p className='mb-3 text-sm font-medium'>TYPE</p>
@@ -70,7 +76,26 @@ const Collection = () => {
 
       </div>
 
+      {/* Right side */}  
+      <div className='flex-1'>
+
+        <div className='flex justify-between text-base sm:text-2xl mb-4'>
+
+          <Title text1={'All'} text2={'COLLECTIONS'} />
+
+          {/* product sort */}
+
+          <select className='border-2 border-gray-300 text-sm px-2'>
+            <option value="relavent">Sort by: Relavent</option>
+            <option value="low-high">Sort by: Low to High</option>
+            <option value="high-low">Sort by: High to Low</option>
+          </select>
+
+
+        </div>
+
       
+    </div>
     </div>
   );
 }
