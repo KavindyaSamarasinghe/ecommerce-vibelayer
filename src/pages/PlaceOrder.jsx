3f1388a332f1faa8a1f90085cc628606ebcbefde
KavindyaSamarasinghe <kavindyasamara@gmail.com>
2025-04-17 14:51:31 +0530

    left side of place-order page

--- src/pages/PlaceOrder.jsx
+++ src/pages/PlaceOrder.jsx
@@ -11,13 +11,39 @@ const PlaceOrder = () => {
 
         </div>
         <div className='flex gap-3'>
+          <input className='border border-gray=300 rounded py-1.5 px-3.5 w-full' type="text" placeholder= 'First name' />
+          <input className='border border-gray=300 rounded py-1.5 px-3.5 w-full' type="text" placeholder= 'Last name' />
+          </div>
+
+          <div>
+          <input className='border border-gray=300 rounded py-1.5 px-3.5 w-full' type="email" placeholder= 'Email Address' />
+          </div>
+          <div>
+          <input className='border border-gray=300 rounded py-1.5 px-3.5 w-full' type="text" placeholder= 'Street' />
+          </div>
+
+          <div className='flex gap-3'>
+          <input className='border border-gray=300 rounded py-1.5 px-3.5 w-full' type="text" placeholder= 'City' />
+          <input className='border border-gray=300 rounded py-1.5 px-3.5 w-full' type="text" placeholder= 'State' />
+          </div>
+
+          <div className='flex gap-3'>
+          <input className='border border-gray=300 rounded py-1.5 px-3.5 w-full' type="number" placeholder= 'Zipcode' />
+          <input className='border border-gray=300 rounded py-1.5 px-3.5 w-full' type="text" placeholder= 'Country' />
+          </div>
+
+          <div>
+          <input className='border border-gray=300 rounded py-1.5 px-3.5 w-full' type="number" placeholder= 'Phone' />
+
+          </div>
+
 
         </div>
 
 
       </div>
       
-    </div>
+   
   );
 }
 
