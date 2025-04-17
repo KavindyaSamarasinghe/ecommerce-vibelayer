b6c7dece5e99d2b0d69caaa86038e8c9a54ffa74
KavindyaSamarasinghe <kavindyasamara@gmail.com>
2025-04-17 21:26:15 +0530

    About Us Page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Title from '../components/Title';
 import { assets } from '../assets/assets';
+import NewsletterBox from '../components/NewsletterBox';
 
 const About = () => {
   return (
@@ -30,6 +31,29 @@ const About = () => {
             unique style and values.</p>
         </div>
       </div>
+      <div className='text-xl py-4'>
+        <Title text1={'WHY'} text2={'CHOOSE US'} />
+      </div>
+      <div className='flex flex-col md:flex-row text-sm mb-20'>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Quality Assurance:</b>
+          <p className='text-gray-600'>Every VibeLayer piece undergoes rigorous testing to ensure premium quality and lasting durability.</p>
+        </div>
+
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Convenience:</b>
+          <p className='text-gray-600'>Shop effortlessly with intuitive navigation, fast shipping, and hassle-free returns on all orders.</p>
+        </div>
+
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Exceptional Customer Service:</b>
+          <p className='text-gray-600'>Our dedicated team provides personalized support to ensure your complete satisfaction with every purchase.</p>
+        </div>
+
+      </div>
+
+      <NewsletterBox />
+
       
     </div>
   );
